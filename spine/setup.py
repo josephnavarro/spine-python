1821cfeeb23fd1ad0debd57dd5574c960b63d460
Joseph Navarro <josephnavarro@yandex.com>
2023-02-25 00:38:16 -0800

    Update for 3.10

--- spine/setup.py
+++ spine/setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-
 from setuptools import setup
 
-setup(name='spine-python',
-      version='1.0b9',
-      description='A Pure Python Spine runtime.',
-      author='Terry Simons',
-      author_email='terry.simons@gmail.com',
-      url='https://github.com/terrysimons/spine-python/spine-python',
-      package_dir={'spine': 'src'},
-      packages=['spine', 'spine.Atlas', 'spine.Animation'],
-      classifiers=['License :: OSI Approved :: BSD License']
-     )
+setup(
+    name='spine-python',
+    version='1.0b9',
+    description='A Pure Python Spine runtime.',
+    author='Joey Navarro',
+    author_email='therealjoeynavarro@aol.com',
+    url='https://github.com/josephnavarro/spine-python/spine',
+    package_dir={'spine': 'src'},
+    packages=['spine', 'spine.Atlas', 'spine.Animation'],
+    classifiers=['License :: OSI Approved :: BSD License']
+)
